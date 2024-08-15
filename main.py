56d55355c7f20a807c83023dc03b487602d50728
Andrei Seabra <andreiamasilio@gmail.com>
2024-08-15 15:07:42 -0300

    adição da página de calculadora de queima de kcal

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ window.iconbitmap("Assets/Images/Icon.ico")
 window.geometry("405x720")
 
 # Page canvas
-page = Canvas(window, width="405", height="655", bg="#F8F8F8")
+page = Canvas(window, width=405, height=655, bg="#F8F8F8")
 page.pack()
 
 # Bottom bar canvas
-bottomBar = Canvas(window, width="405", height="65", bg="#DCDCDC")
+bottomBar = Canvas(window, width=405, height=65, bg="#DCDCDC")
 bottomBar.pack()
 
 # Bottom Bar Buttons
@@ -35,13 +35,13 @@ for i, icon in enumerate(icons):
     buttonIcon = PhotoImage(file=icon)
 
     button = Button(bottomBar, image=buttonIcon, bd=0, anchor="center")
-    button.grid(row=0, column=i, ipadx="3", ipady="3")
+    button.grid(row=0, column=i, ipadx=3, ipady=3)
 
     button.image = buttonIcon # Avoids problems with iteration
 
     buttons.append(button)
 
-bmiCalculator(page)
+goalPage(page)
 
 # Runs the window
 window.mainloop()
--- myapp.py
+++ myapp.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
-# Interface methods
+# Interface
+
+# Methods
 
 def cleanCanvas(canvas: Canvas):
     """
@@ -11,6 +13,8 @@ def cleanCanvas(canvas: Canvas):
 
     for widget in widgets:
         canvas.delete(widget)
+
+# Guis
 
 def homePage(canvas: Canvas = "page"):
     """
@@ -27,41 +31,108 @@ def homePage(canvas: Canvas = "page"):
     canvas.create_text(24, 233, anchor="nw", font=("Inter", 18), text="Seja bem-vindo(a), ") # App title
     canvas.create_text(24, 270, anchor="nw", font=("Inter", 15), text="ao aplicativo que te mantém em forma!") # App title
 
-def kcalCalculatorPage():
-    ...
 
-def bmiCalculator(canvas: Canvas = "page"):
+
+def kcalCalculatorPage(canvas: Canvas = "page"):
+    """
+        Inserts the kcal calculator to the page canvas.
+    """
+
+    cleanCanvas(canvas)
+
+    # References
+    timer = PhotoImage(file="Assets/Images/Timer.png")
+    symbol = PhotoImage(file="Assets/Images/BurnedCalories.png")
+    playButton = PhotoImage(file="Assets/Images/PlayButton.png")
+    pauseButton = PhotoImage(file="Assets/Images/PauseButton.png")
+
+    sportsIcons = [
+        "Assets/Images/WalkingButton.png",
+        "Assets/Images/CyclingButton.png",
+        "Assets/Images/RunningButton.png"
+    ]
+
+    # Sports canvas
+    canvasSports = Canvas(canvas, width=303, height=75, bg="#F8F8F8")
+    canvasSports.place(x=51, y=66)
+
+    # Sports buttons
+    buttons = []
+
+    for i, icon in enumerate(sportsIcons):
+        buttonIcon = PhotoImage(file=icon)
+
+        button = Button(canvasSports, image=buttonIcon, bd=0, anchor="center")
+        button.grid(row=0, column=i, ipadx=15)
+
+        button.image = buttonIcon # Avoids problems with iteration
+
+        buttons.append(button)
+
+    # Timer display
+    canvas.create_image(90, 235, anchor="nw", image=timer)
+    canvas.timer = timer # Avoids calling function problems
+
+    canvas.create_text(140, 250, anchor="nw", font=("Inter", 36), text="00:00") # Timer
+
+    # Toogler buttton
+    tooglerButton = Button(canvas, image=playButton, bd=0, anchor="nw")
+    tooglerButton.place(x=158, y=417)
+    canvas.playButton = playButton # Avoids calling function problems
+
+    # Burned Kcal counter
+    canvas.create_image(72, 543, anchor="nw", image=symbol)
+    canvas.symbol = symbol # Avoids calling function problems
+
+    canvas.create_text(134, 555, anchor="nw", font=("Inter", 28, "bold"), text="1.600 cal")
+
+
+
+def bmiCalculatorPage(canvas: Canvas = "page"):
     """
         Inserts the bmi calculator to the page canvas.
     """
     
     cleanCanvas(canvas)
 
-    # references
+    # References
     entryImg = PhotoImage(file="Assets/Images/Entry.png") # Entry background
     buttonImg = PhotoImage(file="Assets/Images/CalculateButton.png") # Calculate button
 
     canvas.create_text(48, 100, anchor="nw", font=("Inter", 25, "bold"), text="Calculadora de IMC") # Title
 
-    # Weight stuff
-    canvas.create_text(88, 208, anchor="nw", font=("Inter", 16, "bold"), text="Peso (kg)") # Weight's entry title
+    # Weight's and Height's entry
+    entries = []
+
+    entriesDict = {
+        "Peso (kg)": [208, 235, 237.5],
+        "Altura (m)": [315, 342, 344.5]
+    }
+
+    # Entry stuff
+    for entry in entriesDict:
+        canvas.create_text(88, entriesDict[entry][0], anchor="nw", font=("Inter", 16, "bold"), text=entry) # Entry title
     
-    canvas.create_image(78, 235, anchor="nw", image=entryImg) # Weight's entry image
-    canvas.entryImg = entryImg # Avoids calling function problems
+        canvas.create_image(78, entriesDict[entry][1], anchor="nw", image=entryImg) # Entry image
+        canvas.entryImg = entryImg # Avoids calling function problems
 
-    weightEntry = Entry(canvas, border=8, bd=0, fg="black", font=("Inter", 20), highlightbackground="#DCDCDC", background="#DCDCDC")
-    canvas.create_window(85, 237.5, anchor="nw", width=235, height=45, window=weightEntry) # Weight's entry
+        guiEntry = Entry(canvas, bd=0, fg="black", font=("Inter", 20), highlightbackground="#DCDCDC", background="#DCDCDC")
+        canvas.create_window(85, entriesDict[entry][2], anchor="nw", width=235, height=45, window=guiEntry) # Entry
 
-    # Height stuff
-    canvas.create_text(88, 315, anchor="nw", font=("Inter", 16, "bold"), text="Altura (m)") # Height's entry title
-
-    canvas.create_image(78, 342, anchor="nw", image=entryImg) # Weight's entry image
-    canvas.entryImg = entryImg # Avoids calling function problems
-
-    heightEntry = Entry(canvas, border=8, bd=0, fg="black", font=("Inter", 20), highlightbackground="#DCDCDC", background="#DCDCDC")
-    canvas.create_window(85, 344.5, anchor="nw", width=235, height=45, window=heightEntry) # Height's entry
+        entries.append(guiEntry)
 
     # Button stuff
     calculateButton = Button(canvas, image=buttonImg, bd=0)
     canvas.create_window(121, 415, anchor="nw", width=165, height=55, window=calculateButton) # Calculate Button
     canvas.buttonImg = buttonImg # Avoids calling function problems
+
+
+
+def goalPage(canvas: Canvas = "page"):
+    """
+        Inserts the goals to the page canvas.
+    """
+
+    cleanCanvas(canvas)
+
+    # References
